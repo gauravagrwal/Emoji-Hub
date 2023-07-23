e711f9af9cf92fa62ed3f6469824ad43d816cf51
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-07-23 15:35:36 +0530

    feat: (R) fetching data from endpoint using Axios
    https://axios-http.com/docs/intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,22 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function App() {
+  const [data, setData] = useState([]);
+  async function getData() {
+    try {
+      const response = await axios.get('https://emojihub.yurace.pro/api/all');
+      console.info(response);
+      setData(response.data);
+    }
+    catch (error) {
+      console.error(error);
+    }
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
     </>
