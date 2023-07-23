ab734e29ccde2eb9f35e8e39de9851141b919b8c
Gaurav Agrawal <gauravag.1005@gmail.com>
2023-07-23 17:47:46 +0530

    feat: filtering the emojis by "category"

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,43 @@
+import { useState } from 'react';
+
 import Emoji from './components/Emoji';
 
 export default function Home({ data }) {
+  //#region Category
+  const [category, setCategory] = useState('all');
+
+  let categorizedData = data.filter((item) => {
+    if (category === 'all')
+      return true;
+    else
+      return item.category === category;
+  });
+
+  function changeCategory(e) {
+    setCategory(e.target.value);
+  }
+  //#endregion
+
   return (
     <>
+      <div className='flex justify-center'>
+        <select onChange={(e) => { changeCategory(e) }} className='p-2 rounded-md w-3/4'>
+          <option selected disabled>Choose a cateogry</option>
+          <option value='all'>All</option>
+          <option value='smileys and people'>Smileys and People</option>
+          <option value='food and drink'>Food and Drink</option>
+          <option value='animals and nature'>Animals and Nature</option>
+          <option value='travel and places'>Travel and Places</option>
+          <option value='activities'>Activities</option>
+          <option value='objects'>Objects</option>
+          <option value='symbols'>Symbols</option>
+          <option value='flags'>Flags</option>
+        </select>
+      </div>
+
       <div className='flex flex-wrap'>
         {
-          data.map((item) => (
+          categorizedData.map((item) => (
             <Emoji key={item.id} item={item} />
           ))
         }
